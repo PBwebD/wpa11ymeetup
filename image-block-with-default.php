c137325b1aedf1b0d2b3bf0ef1ef635a87b640b8
Danielle Zarcaro <info@pbwebd.com>
2022-08-13 03:44:45 -0400

    Add files to project

---
+++ image-block-with-default.php
@@ -0,0 +1,3 @@
+<!-- wp:image {"width":120,"height":120,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/woman-tshirt.jpg' ) ); ?>" alt="Woman with smoky eye shadow staring into camera in front of a white background." width="120" height="120"/></figure>
+<!-- /wp:image -->
